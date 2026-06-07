2a6f1fada54f3c7a35ecbe13d383f215ff70b71e
msa <shahajahancse@gmail.com>
2026-06-07 17:50:14 +0600

    appl backuo delete

--- application_2026_02_24/views/month_year_salary_report_second.php
+++
@@ -1,3 +0,0 @@
-Second Month and Year :
-
-<td>First Date </td><td>:</td><td> <input type="text" name="firstdate" id="firstdate" style="width:100px;"/></td>
--- application_2026_02_24/views/upload.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-$target_path="D:\xampplite\htdocs\payroll\uploads\photo";
-@move_uploaded_file($_FILES['userfile']['tmp_name'], $target_path)
-
-?>
